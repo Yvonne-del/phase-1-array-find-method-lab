c681df1fb6d02bc9ede8633b56789a778e1050c7
Yvonne-del <ys359375@gmail.com>
2025-03-15 15:15:54 +0300

    find() array iteration

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+
+  function superbowlWin(records){
+    let win = records.find(game =>game.result ==="W")
+    if(win){
+        return win.year;
+    }
+  }
+ 
